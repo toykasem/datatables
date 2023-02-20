261030e2948c8974de513ed1f2d894f6b61175a6
toykasem <toykasemb42@gmail.com>
2023-02-20 14:59:21 +0700

    update

---
+++ application/logs/log-2023-02-20.php
@@ -0,0 +1,65 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+DEBUG - 2023-02-20 08:47:53 --> UTF-8 Support Enabled
+DEBUG - 2023-02-20 08:47:53 --> No URI present. Default controller set.
+DEBUG - 2023-02-20 08:47:53 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2023-02-20 08:47:53 --> Total execution time: 0.0341
+DEBUG - 2023-02-20 08:47:53 --> UTF-8 Support Enabled
+DEBUG - 2023-02-20 08:47:53 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2023-02-20 08:47:53 --> Total execution time: 0.0264
+DEBUG - 2023-02-20 08:47:55 --> UTF-8 Support Enabled
+DEBUG - 2023-02-20 08:47:55 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2023-02-20 08:47:55 --> Total execution time: 0.0403
+DEBUG - 2023-02-20 08:47:56 --> UTF-8 Support Enabled
+DEBUG - 2023-02-20 08:47:56 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2023-02-20 08:47:56 --> Total execution time: 0.0292
+DEBUG - 2023-02-20 08:47:57 --> UTF-8 Support Enabled
+DEBUG - 2023-02-20 08:47:57 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2023-02-20 08:47:57 --> Total execution time: 0.0344
+DEBUG - 2023-02-20 08:47:58 --> UTF-8 Support Enabled
+DEBUG - 2023-02-20 08:47:58 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2023-02-20 08:47:58 --> Total execution time: 0.0338
+DEBUG - 2023-02-20 08:47:59 --> UTF-8 Support Enabled
+DEBUG - 2023-02-20 08:47:59 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2023-02-20 08:47:59 --> Total execution time: 0.0231
+DEBUG - 2023-02-20 08:48:00 --> UTF-8 Support Enabled
+DEBUG - 2023-02-20 08:48:00 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2023-02-20 08:48:00 --> Total execution time: 0.0192
+DEBUG - 2023-02-20 08:48:01 --> UTF-8 Support Enabled
+DEBUG - 2023-02-20 08:48:01 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2023-02-20 08:48:01 --> Total execution time: 0.0288
+DEBUG - 2023-02-20 08:48:02 --> UTF-8 Support Enabled
+DEBUG - 2023-02-20 08:48:02 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2023-02-20 08:48:02 --> Total execution time: 0.0350
+DEBUG - 2023-02-20 08:52:51 --> UTF-8 Support Enabled
+DEBUG - 2023-02-20 08:52:51 --> Global POST, GET and COOKIE data sanitized
+ERROR - 2023-02-20 08:52:51 --> 404 Page Not Found: Customer/edit
+DEBUG - 2023-02-20 08:52:53 --> UTF-8 Support Enabled
+DEBUG - 2023-02-20 08:52:53 --> No URI present. Default controller set.
+DEBUG - 2023-02-20 08:52:53 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2023-02-20 08:52:53 --> Total execution time: 0.0188
+DEBUG - 2023-02-20 08:52:53 --> UTF-8 Support Enabled
+DEBUG - 2023-02-20 08:52:53 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2023-02-20 08:52:53 --> Total execution time: 0.0428
+DEBUG - 2023-02-20 08:52:54 --> UTF-8 Support Enabled
+DEBUG - 2023-02-20 08:52:54 --> Global POST, GET and COOKIE data sanitized
+ERROR - 2023-02-20 08:52:54 --> 404 Page Not Found: Customer/edit
+DEBUG - 2023-02-20 08:52:55 --> UTF-8 Support Enabled
+DEBUG - 2023-02-20 08:52:55 --> No URI present. Default controller set.
+DEBUG - 2023-02-20 08:52:55 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2023-02-20 08:52:55 --> Total execution time: 0.0314
+DEBUG - 2023-02-20 08:52:55 --> UTF-8 Support Enabled
+DEBUG - 2023-02-20 08:52:55 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2023-02-20 08:52:55 --> Total execution time: 0.0236
+DEBUG - 2023-02-20 08:52:58 --> UTF-8 Support Enabled
+DEBUG - 2023-02-20 08:52:58 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2023-02-20 08:52:58 --> Total execution time: 0.0183
+DEBUG - 2023-02-20 08:53:02 --> UTF-8 Support Enabled
+DEBUG - 2023-02-20 08:53:02 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2023-02-20 08:53:02 --> Total execution time: 0.0393
+DEBUG - 2023-02-20 08:53:04 --> UTF-8 Support Enabled
+DEBUG - 2023-02-20 08:53:04 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2023-02-20 08:53:04 --> Total execution time: 0.0197
+DEBUG - 2023-02-20 08:53:05 --> UTF-8 Support Enabled
+DEBUG - 2023-02-20 08:53:05 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2023-02-20 08:53:05 --> Total execution time: 0.0326
